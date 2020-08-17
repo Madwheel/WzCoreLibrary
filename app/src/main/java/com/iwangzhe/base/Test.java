9e34270ebd0323c581104cd2e3fd2a2469c2c668
Madwheel <374211103@qq.com>
2020-08-17 08:29:11 +0800

    第一版

---
+++ app/src/main/java/com/iwangzhe/base/Test.java
@@ -0,0 +1,19 @@
+package com.iwangzhe.base;
+
+import com.iwangzhe.wzcorelibrary.INetHttp;
+import com.iwangzhe.wzcorelibrary.IResCallback;
+
+import java.util.Map;
+
+/**
+ * author : 亚辉
+ * e-mail : 2372680617@qq.com
+ * date   : 2020/8/178:24
+ * desc   :
+ */
+public class Test implements INetHttp {
+    @Override
+    public void reqGetResByWzApi(String url, Map<String, String> params, IResCallback callback) {
+        
+    }
+}
