fd16de74a672ab811cb3b37edab4809c5f419854
Madwheel <374211103@qq.com>
2020-09-28 09:44:25 +0800

    add

---
+++ WzCoreLibrary/src/main/java/com/iwangzhe/wzcorelibrary/base/app/ControlApp.java
@@ -0,0 +1,15 @@
+package com.iwangzhe.wzcorelibrary.base.app;
+
+import com.iwangzhe.wzcorelibrary.base.main.ModMain;
+
+/**
+ * author : 亚辉
+ * e-mail : 2372680617@qq.com
+ * date   : 2020/9/213:18
+ * desc   :
+ */
+public class ControlApp extends App {
+    protected ControlApp(ModMain main) {
+        super(main);
+    }
+}
---
+++ WzCoreLibrary/src/main/java/com/iwangzhe/wzcorelibrary/base/inter/IResCallback.java
@@ -0,0 +1,17 @@
+package com.iwangzhe.wzcorelibrary.base.inter;
+
+import com.iwangzhe.wzcorelibrary.base.CommonRes;
+
+import java.io.IOException;
+
+/**
+ * author : 亚辉
+ * e-mail : 2372680617@qq.com
+ * date   : 2020/9/213:22
+ * desc   :
+ */
+public interface IResCallback<T> {
+    void onFailure(IOException e);
+
+    void onFinish(CommonRes<T> res);
+}
